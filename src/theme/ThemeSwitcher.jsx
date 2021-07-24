f2319635d19d8cbdce80faacab2f06c3f4ed731b
Dan Wahlin <dwahlin@xmlforasp.net>
2021-07-24 15:53:25 -0700

    Add folders/files

---
+++ src/theme/ThemeSwitcher.jsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import ThemeContext from './ThemeContext';
+
+function ThemeSwitcher() {
+  const { theme, setTheme } = useContext(ThemeContext)
+
+  return (
+    <div className="theme-switcher">
+      <button 
+        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+        className="button-theme"
+      >
+        Toggle Theme
+      </button>
+    </div>
+  )
+}
+
+export default ThemeSwitcher
